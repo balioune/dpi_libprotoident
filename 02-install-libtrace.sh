4c25edc4d25f053781293838f73e79fbb8ae7ad4
Alioune BA <alioune.ba@orange.com>
2016-10-01 22:39:41 +0000

    Libtrace for NSH protocol

--- 02-install-libtrace.sh
+++ 02-install-libtrace.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 
-<<<<<<< HEAD
 tar -jxf libtrace-nsh.tar.bz2
-=======
-tar -jxf libtrace-nsh-3.0.22.tar.bz2
->>>>>>> bce4d44008c8334474838caf2426a3008a41bba1
 
 cd libtrace-3.0.22
  
